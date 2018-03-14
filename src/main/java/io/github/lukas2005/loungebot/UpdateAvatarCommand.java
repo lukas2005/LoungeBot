5cd288e8893a5c667bde6760c132b75c7accc3f1
lukas2005 <lukas2005.38@gmail.com>
2018-03-14 16:38:45 +0100

    tessts

--- src/main/java/io/github/lukas2005/loungebot/UpdateAvatarCommand.java
+++ src/main/java/io/github/lukas2005/loungebot/UpdateAvatarCommand.java
@@ -4,6 +4,7 @@ import io.github.lukas2005.loungebot.modules.Command;
 import org.javacord.DiscordApi;
 import org.javacord.entity.channel.ServerTextChannel;
 import org.javacord.entity.server.Server;
+import org.javacord.entity.user.User;
 import org.javacord.event.message.MessageCreateEvent;
 
 import java.net.MalformedURLException;
@@ -19,11 +20,13 @@ public class UpdateAvatarCommand implements Command {
 			String[] messageContentSplit = messageContent.split(" ");
 			DiscordApi api = e.getApi();
 
-			if ((Main.checkForCommand(messageContent, "avatar", server, api)) && e.getMessage().getAuthor().isBotOwner()) {
-				try {
-					api.updateAvatar(new URL(messageContentSplit[messageContentSplit.length - 1]));
-				} catch (MalformedURLException e1) {
-					textChannel.sendMessage("Sorry but you provided a Malformed url");
+			if ((Main.checkForCommand(messageContent, "avatar", server, api)) || (Main.checkForCommand(messageContent, "av", server, api))) {
+				if (e.getMessage().getAuthor().isBotOwner() && e.getMessage().getMentionedUsers().size() == 0) {
+					try {
+						api.updateAvatar(new URL(messageContentSplit[messageContentSplit.length - 1]));
+					} catch (MalformedURLException e1) {
+						textChannel.sendMessage("Sorry but you provided a Malformed url");
+					}
 				}
 			}
 		}
--- src/main/java/io/github/lukas2005/loungebot/UpdateCommand.java
+++ src/main/java/io/github/lukas2005/loungebot/UpdateCommand.java
@@ -24,7 +24,7 @@ public class UpdateCommand implements Command {
 				if (OsUtils.isWindows()) {
 					//new ProcessBuilder("cmd", "/c", "echo", "test").inheritIO().start();
 				} else {
-					new ProcessBuilder("bash", "-c", "screen", "../update.sh").inheritIO().start();
+					new ProcessBuilder("bash", "-c", "screen", "-d", "-m", "../update.sh").inheritIO().start();
 				}
 
 				//System.exit(0);
